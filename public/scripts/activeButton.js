b8ff6ecde4802e92b6f3fb57bda58d20647ae45f
Janetquispeu <janetquispeu@gmail.com>
2017-03-14 15:17:19 +0000

    finalizando prueba

--- public/scripts/activeButton.js
+++ public/scripts/activeButton.js
@@ -1,10 +1,5 @@
 function activeButton (event) {
-	var inputSearch = $("#inputSearch").val();
   var evento = window.event || elEvento;
   $("#btnSearch").attr("disabled", false);
-  if(inputSearch == 0){
- 		$("#btnSearch").attr("disabled", true);  	
-  }
 }
-
 
