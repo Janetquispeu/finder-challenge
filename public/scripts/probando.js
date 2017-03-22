04b6fb6862a0c35cf77efcb9cbf30f2d5e2e6896
Janetquispeu <janetquispeu@gmail.com>
2017-03-22 17:42:23 +0000

    writing all javascript

--- public/scripts/probando.js
+++ public/scripts/probando.js
@@ -3,53 +3,75 @@
 /// <reference path="../../node_modules/@types/underscore/underscore.d.ts" />
 var modalFinder = (function () {
     function modalFinder() {
+        var _this = this;
         this.json_file = "/books-schema.json";
+        this.inputSearch = document.getElementById("#inputSearch");
+        this.eliminate = document.getElementById("#delete");
+        this["delete"] = function (event) {
+            var parentTemporal = event.target.parentElement;
+            _this.findParent(parentTemporal);
+        };
+        this.findParent = function (parentTemporal) {
+            console.log("parentTemporal", parentTemporal);
+            if (parentTemporal.classList.contains('listData')) {
+                var listdata = document.getElementsByClassName("listData");
+                console.log(listdata);
+                listdata.remove();
+            }
+            else {
+                var parentTemp = parentTemporal.parentElement;
+                console.log("parentTemp", parentTemp);
+                _this.findParent(parentTemp);
+            }
+        };
     }
-    modalFinder.prototype.searchForm = function () {
+    modalFinder.prototype.callingAJax = function (callback) {
         var xobj = new XMLHttpRequest();
         xobj.overrideMimeType("application/json");
         xobj.open("GET", this.json_file, true);
         xobj.onreadystatechange = function (responseText) {
             if (xobj.readyState == 4 && xobj.status == 200) {
                 var content = JSON.parse(xobj.responseText);
-                var list = content.data.map(function (object, index) {
-                    return object.title;
-                });
-                new Awesomplete(document.querySelector("#ajax-example input"), { list: list });
+                callback.call(this, content);
             }
         };
         xobj.send();
     };
-    modalFinder.prototype.callingList = function () {
+    modalFinder.prototype.searchForm = function (content) {
+        var list = content.data.map(function (object, index) {
+            return object.title;
+        });
+        new Awesomplete(document.querySelector("#ajax-example input"), { list: list });
+    };
+    modalFinder.prototype.callingList = function (content) {
         var tplTemplate = document.getElementById("tpl-template");
         var compiled = _.template(tplTemplate.innerHTML);
-        var xobj = new XMLHttpRequest();
-        $.getJSON(this.json_file, function (value) {
-            $.each(value.entities.saved, function (val, object) {
-                var htmlCompiled = compiled({
-                    mivariable_label: object.label
-                });
-                $(htmlCompiled).appendTo(".data");
+        var name = content.entities.saved;
+        var data = document.getElementsByClassName("data");
+        var nameBook = name.map(function (object, index) {
+            var htmlCompiled = compiled({
+                mivariable_label: object.label
             });
+            $(htmlCompiled).appendTo(data);
         });
     };
     modalFinder.prototype.activeButton = function (event) {
         var element = document.getElementById("btnSearch");
-        var inputSearch = $("#inputSearch").val();
+        var inputSearch = document.getElementById("inputSearch");
+        var inputSearchLength = inputSearch.value.length;
         var evento = window.event;
         element.disabled = false;
-        if (inputSearch == 0) {
+        if (inputSearchLength == 0) {
             element.disabled = true;
         }
-    };
-    modalFinder.prototype["delete"] = function () {
-        var listData = $(this).parent().parent();
-        listData.remove();
     };
     return modalFinder;
 }());
 var root = new modalFinder;
-root.searchForm();
-root.callingList();
-$("#inputSearch").keyup(root.activeButton);
-$(document).on("click", ".delete", root["delete"]);
+console.log(this.inputSearch);
+console.log(this.eliminate);
+root.callingAJax(root.searchForm);
+root.callingAJax(root.callingList);
+this.inputSearch.addEventListener("keyup", root.activeButton);
+//$(document).on("click",".delete",root.delete);
+document.addEventListener('click', root["delete"]);
--- src/scripts/probando.ts
+++ src/scripts/probando.ts
@@ -4,59 +4,80 @@
 
 class modalFinder{
   private json_file:string = "/books-schema.json";
-  
-  
-  public searchForm(){
-    var xobj= new XMLHttpRequest();
+  public inputSearch : HTMLInputElement = <HTMLInputElement>document.getElementById("#inputSearch");
+  public eliminate : HTMLElement = <HTMLAnchorElement>document.getElementById("#delete");
+
+  public callingAJax(callback:any):any{
+    let xobj = new XMLHttpRequest();
     xobj.overrideMimeType("application/json");
     xobj.open("GET",this.json_file,true);
     xobj.onreadystatechange = function(responseText){
       if(xobj.readyState == 4 && xobj.status == 200){
-        var content = JSON.parse(xobj.responseText);
-        var list = content.data.map(function(object:any, index:number) { 
-          return object.title;
-        });
-        new Awesomplete(<HTMLElement>document.querySelector("#ajax-example input"),{ list: list });
+        let content = JSON.parse(xobj.responseText);
+        callback.call(this, content);
       }
-    };
+    }
     xobj.send();
   }
+  public searchForm(content:any):any{
+    let list = content.data.map(function(object:any, index:number){
+      return object.title;
+    });
+    new Awesomplete(<HTMLElement>document.querySelector("#ajax-example input"),{ list: list });
+  }
   
-  public callingList(){
-    var tplTemplate = document.getElementById("tpl-template");
-    var compiled = _.template(tplTemplate.innerHTML);
-    var xobj= new XMLHttpRequest();
-    $.getJSON(this.json_file,function(value){
-      $.each(value.entities.saved,function(val:number,object:any){
-        var htmlCompiled = compiled({
-          mivariable_label : object.label
-        });
+  public callingList(content:any){
+    let tplTemplate = document.getElementById("tpl-template");
+    let compiled    = _.template(tplTemplate.innerHTML);
+    let name        = content.entities.saved;
+    let data:any    = (<HTMLScriptElement[]><any>document.getElementsByClassName("data"));
 
-        $(htmlCompiled).appendTo(".data");
+    let nameBook=name.map(function(object:any, index:number):any{
+      let htmlCompiled : any = compiled({
+        mivariable_label : object.label
       });
+      $(htmlCompiled).appendTo(data);
     });
   }
   
   public activeButton(event:any):any{
-    var element = <HTMLButtonElement> document.getElementById("btnSearch");
-    var inputSearch = $("#inputSearch").val();
-    var evento = window.event;
+    let element = <HTMLButtonElement> document.getElementById("btnSearch");
+    let inputSearch = <HTMLInputElement>document.getElementById("inputSearch");
+    let inputSearchLength = inputSearch.value.length;
+    let evento = window.event;
     element.disabled = false;
     
-    if(inputSearch == 0){
+    if(inputSearchLength == 0){
       element.disabled = true;
     }
   }
   
+  public delete = (event: any) => {
+    let parentTemporal = event.target.parentElement;
+    this.findParent(parentTemporal);
+  }
 
-  public delete(){
-    var listData = $(this).parent().parent();
-    listData.remove();
+  public findParent = (parentTemporal:any):any => {
+    console.log("parentTemporal", parentTemporal);
+    if(parentTemporal.classList.contains('listData')){
+      var listData:any = <HTMLScriptElement[]><any>document.getElementsByClassName("listData");
+      console.log(listData);
+      listData.remove();
+    } else {
+      let parentTemp= parentTemporal.parentElement;
+      console.log("parentTemp", parentTemp);
+      this.findParent(parentTemp);
+    }
   }
 }
 
 var root= new modalFinder;
-root.searchForm();
-root.callingList();
-$("#inputSearch").keyup(root.activeButton);
-$(document).on("click",".delete",root.delete);
+
+console.log(this.inputSearch);
+console.log(this.eliminate);
+root.callingAJax(root.searchForm);
+root.callingAJax(root.callingList);
+this.inputSearch.addEventListener("keyup",root.activeButton);
+//$(document).on("click",".delete",root.delete);
+document.addEventListener('click', root.delete);
+
